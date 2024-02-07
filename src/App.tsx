5312ccbb95a2a791fecdc648005d1a422e2bcb6c
Mateus V. Farias <me@mateusfarias.com.br>
2024-02-07 06:46:01 -0400

    feat: introduce note card

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import Logo from "@/assets/logo.svg";
-import { NewNoteCard } from "@/components/new-note-card";
-import { NoteCard } from "@/components/new-card";
+import Logo from '@/assets/logo.svg';
+import { NoteCard } from '@/components/new-card';
+import { NewNoteCard } from '@/components/new-note-card';
 
 export default function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto my-12 max-w-6xl space-y-6">
       <img src={Logo} alt="NLW Expert" />
 
       <form className="w-full">
         <input
           type="text"
           placeholder="Busque em suas notas..."
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+          className="placeholder:text-state-500 w-full bg-transparent text-3xl font-semibold tracking-tight outline-none"
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard
+          note={{
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+            date: new Date(),
+          }}
+        />
       </div>
     </div>
   );
