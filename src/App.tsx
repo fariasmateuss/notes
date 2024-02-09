ded9cf978570bb9c8e68876d56815f7a804ea2a9
Mateus V. Farias <me@mateusfarias.com.br>
2024-02-08 23:00:56 -0400

    refactor: search notes updates as transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { useLocalStorage } from 'usehooks-ts';
 
 import Logo from '@/assets/logo.svg';
 import { NewNoteCard } from '@/components/new-note-card';
 import { NoteCard } from '@/components/note-card';
+import { NOTES_STORAGE_KEY } from './constants/storage';
 
 type Note = {
   id: string;
@@ -12,15 +14,8 @@ type Note = {
 
 export default function App() {
   const [search, setSearch] = React.useState('');
-  const [notes, setNotes] = React.useState<Note[]>(() => {
-    const notesOnStorage = localStorage.getItem('notes');
-
-    if (notesOnStorage) {
-      return JSON.parse(notesOnStorage);
-    }
-
-    return [];
-  });
+  const [notes, setNotes] = useLocalStorage(NOTES_STORAGE_KEY, [] as Note[]);
+  const [_, startTransition] = React.useTransition();
 
   const onNoteCreated = (content: string) => {
     const newNote = {
@@ -32,22 +27,18 @@ export default function App() {
     const notesArray = [newNote, ...notes];
 
     setNotes(notesArray);
-
-    localStorage.setItem('notes', JSON.stringify(notesArray));
   };
 
   const onNoteDeleted = (id: string) => {
     const notesArray = notes.filter((note) => note.id !== id);
 
     setNotes(notesArray);
-
-    localStorage.setItem('notes', JSON.stringify(notesArray));
   };
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const query = e.target.value;
 
-    setSearch(query);
+    startTransition(() => setSearch(query));
   };
 
   const filteredNotes =
@@ -75,7 +66,7 @@ export default function App() {
       <div className="grid auto-rows-[250px] grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        {filteredNotes.map((note) => {
+        {filteredNotes?.map((note) => {
           return (
             <NoteCard onNoteDeleted={onNoteDeleted} key={note.id} note={note} />
           );
---
+++ src/constants/storage.ts
@@ -0,0 +1,1 @@
+export const NOTES_STORAGE_KEY = '@nlwexperts:notes';
